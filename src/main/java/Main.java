14b11728f0c52f97ffd38a1dafbe5156a8451517
Gregory <gregory.ac@estudiantec.cr>
2021-03-29 10:53:10 -0600

    Agregué archivo de registro de errores

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    // Al logger se le debe buscar un archivo de configuración
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        try {
+            System.out.println("Hola mundo");
+            int i = 0;
+            int b = 10 / i;
+        } catch (Exception e){
+            logger.error("Algo salió mal", e);
+        }
+
     }
 }
-// ver en 1:02:04
